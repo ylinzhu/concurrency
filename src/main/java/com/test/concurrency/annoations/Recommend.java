51ad55489340380806e1bb8b75855450cc6a4b79
yuanlinzhu <yuanlinzhu1995@gmail.com>
2018-12-26 22:19:56 +0800

    add annoations second

---
+++ src/main/java/com/test/concurrency/annoations/Recommend.java
@@ -0,0 +1,17 @@
+package com.test.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Designation:NotRecommend
+ * @Author: Yuanlinzhu
+ * @Date: 18-12-26 下午10:15
+ * @Version: 1.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+}
